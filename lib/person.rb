8b5264de48fcefa733fc96ad5781b4d21c9963a1
victoria633 <victoria6411@hotmail.com>
2018-06-08 23:32:52 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,34 @@
-class Person 
-def person = (adele_golderg,alan_kay)
-  @this_person 
+class Person
+def name=(person_name)
+@this_perspn_fido = person_name
 end 
-  def person 
-    @this_person
-  end 
+def name
+  @this_dog_fido
+end
+
+def name2= (person_name_2)
+  @this_dog_snoopy = person_name_2
 end 
-    
+def name2
+  @this_dog_snoopy
+end
+  
+  def name3= (dog_name_3)
+    @this_dog_lassie =dog_name_3
+  end
+  def name3
+    @this_dog_lassie
+  end
+end 
+
+fido = Dog.new 
+fido.name = "Fido"
+puts fido.name 
+
+snoopy = Dog.new
+snoopy.name2 = "Snoopy"
+puts snoopy.name2
+
+lassie = Dog.new
+lassie.name3 = "Lassie"
+puts lassie.name3
