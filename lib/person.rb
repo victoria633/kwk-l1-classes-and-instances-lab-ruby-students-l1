00faca8fea68b5de65832750655006becc8f08db
victoria633 <victoria6411@hotmail.com>
2018-06-08 23:39:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,34 +1,23 @@
 class Person
 def name=(person_name)
-@this_perspn_fido = person_name
+@this_adele_goldberg = person_name
 end 
 def name
-  @this_dog_fido
+  @this_adele_goldberg
 end
 
 def name2= (person_name_2)
-  @this_dog_snoopy = person_name_2
+  @this_alan_kay = person_name_2
 end 
 def name2
-  @this_dog_snoopy
+  @this_alan_kay
 end
-  
-  def name3= (dog_name_3)
-    @this_dog_lassie =dog_name_3
-  end
-  def name3
-    @this_dog_lassie
-  end
 end 
 
-fido = Dog.new 
+adele_goldberg= Person.new 
 fido.name = "Fido"
 puts fido.name 
 
-snoopy = Dog.new
+alan_kay= Person.new
 snoopy.name2 = "Snoopy"
 puts snoopy.name2
-
-lassie = Dog.new
-lassie.name3 = "Lassie"
-puts lassie.name3
